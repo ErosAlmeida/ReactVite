962ae265aad0031b52d4bcb9166ef327a487a691
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-05-10 21:16:06 -0400

    input

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ export function App() {
       <Container>
         <form className = 'form' action=''>
             <div className="formRow">
-                  <DefaultInput id='meuInput' type = "text"/>
+                  <DefaultInput labelText="TESTANDO MONSTRO" id='meuInput' type = "text" title="NICEEE"/>
             </div>
 
             <div className="formRow">
