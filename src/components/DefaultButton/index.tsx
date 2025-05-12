af141e147f787f485fc5d5ac3a15a97040f55425
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-05-12 16:43:02 -0400

    DefaultButton

--- src/components/DefaultButton/index.tsx
+++ src/components/DefaultButton/index.tsx
@@ -1,19 +1,20 @@
-import React from "react";
-import styles from "./styles.module.css";
+import styles from './styles.module.css';
 
 type DefaultButtonProps = {
- icon: React.ReactNode
+  icon: React.ReactNode;
+  color?: 'green' | 'red';
 } & React.ComponentProps<'button'>;
 
-
-export function DefaultButton({ icon ,...props} : DefaultButtonProps){
-    return(
-        <>
-                  <button className={styles.button} {...props}>
-                    {icon}
-                  </button>
-
-        </>
-    );
-
+export function DefaultButton({
+  icon,
+  color = 'green',
+  ...props
+}: DefaultButtonProps) {
+  return (
+    <>
+      <button className={`${styles.button} ${styles[color]}`} {...props}>
+        {icon}
+      </button>
+    </>
+  );
 }
