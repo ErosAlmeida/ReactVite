57ac59c3478e6e2a16cd73b84163c6bc996cc5df
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-05-13 18:17:56 -0400

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -9,44 +9,48 @@ import { DefaultInput } from "./components/DefaultInput";
 import { Cycles } from "./components/Cycles";
 import { DefaultButton } from "./components/DefaultButton";
 import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
+import { Footer } from "./components/Footer";
 
 export function App() {
 
-   return <>
+  return <>
 
-      <Container>
-         <Logo/>
-      </Container>
+    <Container>
+      <Logo />
+    </Container>
 
-      <Container>
-        <Menu/>
-      </Container>
+    <Container>
+      <Menu />
+    </Container>
 
-      <Container>
-        <CounterDown/>
-      </Container>
+    <Container>
+      <CounterDown />
+    </Container>
 
-      <Container>
-        <form className = 'form' action=''>
-            <div className="formRow">
-                  <DefaultInput labelText="TESTANDO MONSTRO" id='meuInput' type = "text" title="NICEEE" placeholder="Digite Algo"/>
-            </div>
+    <Container>
+      <form className='form' action=''>
+        <div className="formRow">
+          <DefaultInput labelText="TESTANDO MONSTRO" id='meuInput' type="text" title="NICEEE" placeholder="Digite Algo" />
+        </div>
 
-            <div className="formRow">
-                  <p>Lorem ipsum dolor sit amet.</p>
-            </div>
+        <div className="formRow">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
 
-            <div className="formRow">
-             <Cycles/>
-            </div>
+        <div className="formRow">
+          <Cycles />
+        </div>
 
-            <div className="formRow">
-             <DefaultButton icon = {<PlayCircleIcon/>} color="green"/>
-             <DefaultButton icon = {<StopCircleIcon/>} color="red"/>
-            </div>
-        </form>
-      </Container>
+        <div className="formRow">
+          <DefaultButton icon={<PlayCircleIcon />} />
 
-   </>;
+        </div>
+      </form>
+    </Container>
+
+    <Container>
+      <Footer/>
+    </Container>
+  </>;
 }
 
