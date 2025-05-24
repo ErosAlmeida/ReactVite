2138f6a2c4d47d95415b8c4d04e785536f8a08a0
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-05-24 15:35:26 -0400

    Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,8 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 
-import { Container } from "./components/Container";
-import { Logo } from "./components/Logo";
-import { Menu } from "./components/Menu";
-import { CounterDown } from "./components/CountDown";
-import { Footer } from "./components/Footer";
-import { MainForm } from "./components/MainForm";
-
+import { Home } from "./pages/Home";
 
 export function App() {
-
-  return <>
-
-    <Container>
-      <Logo />
-    </Container>
-
-    <Container>
-      <Menu />
-    </Container>
-
-    <Container>
-      <CounterDown />
-    </Container>
-
-    <Container>
-      <MainForm/>
-    </Container>
-
-    <Container>
-      <Footer/>
-    </Container>
-  </>;
+  return <Home />;
 }
-
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,18 @@
+import { Container } from "../../components/Container";
+import { CounterDown } from "../../components/CountDown";
+import { MainForm } from "../../components/MainForm";
+import { MainTemplate } from "../../templates/MainTemplates";
+
+export function Home() {
+  return (
+    <MainTemplate>
+      <Container>
+        <CounterDown />
+      </Container>
+
+      <Container>
+        <MainForm />
+      </Container>
+    </MainTemplate>
+  );
+}
