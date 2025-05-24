6308b9d41f1d25e28508bb1bc1f15aac1448a1fa
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-05-24 09:42:07 -0400

    Templates

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,8 @@ import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CounterDown } from "./components/CountDown";
-import { DefaultInput } from "./components/DefaultInput";
-import { Cycles } from "./components/Cycles";
-import { DefaultButton } from "./components/DefaultButton";
-import { PlayCircleIcon } from "lucide-react";
 import { Footer } from "./components/Footer";
+import { MainForm } from "./components/MainForm";
 
 
 export function App() {
@@ -29,24 +26,7 @@ export function App() {
     </Container>
 
     <Container>
-      <form className='form' action=''>
-        <div className="formRow">
-          <DefaultInput labelText='task' id='meuInput' type="text" title="NICEEE" placeholder="Digite Algo" />
-        </div>
-
-        <div className="formRow">
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-
-        <div className="formRow">
-          <Cycles />
-        </div>
-
-        <div className="formRow">
-          <DefaultButton icon={<PlayCircleIcon />} />
-
-        </div>
-      </form>
+      <MainForm/>
     </Container>
 
     <Container>
