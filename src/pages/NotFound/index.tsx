649067acbad5c4d1115865beff6ed8a5534f3139
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-05-24 15:39:16 -0400

    NotFound

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,18 @@
+import { Container } from "../../components/Container";
+import { MainTemplate } from "../../templates/MainTemplates";
+
+export function NotFound() {
+  return (
+    <MainTemplate>
+      <Container>
+        <h1>PAGINA NÃO ENCONTRADA</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi
+          facilis, dignissimos saepe quisquam recusandae aliquid. Aperiam, velit
+          architecto distinctio dolore reprehenderit omnis dicta, recusandae
+          nobis tenetur molestiae aliquam, consectetur vel?
+        </p>
+      </Container>
+    </MainTemplate>
+  );
+}
