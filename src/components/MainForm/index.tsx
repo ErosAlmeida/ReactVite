162754bf28010ebdb9a3457b641380cc7bd18cf3
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-05-26 19:26:38 -0400

    handleClick

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -4,15 +4,32 @@ import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 import { HomeProps } from "../../pages/Home";
 
-export function MainForm({ state }: HomeProps) {
+export function MainForm({ state, setState }: HomeProps) {
+  function handleClick() {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: {
+          ...prevState.config,
+          workTime: 34,
+        },
+        formattedSecondsRemaining: "23:34",
+      };
+    });
+  }
+
   return (
     <form className="form" action="">
+      <div>
+        <button type="button" onClick={handleClick}>
+          Clicar
+        </button>
+      </div>
       <div className="formRow">
         <DefaultInput
           labelText="task"
           id="meuInput"
           type="text"
-          title="NICEEE"
           placeholder="Digite Algo"
         />
       </div>
