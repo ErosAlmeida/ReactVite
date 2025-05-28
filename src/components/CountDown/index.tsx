3af5fb06501181fd14d9c8becac2a247caf22e2e
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-05-28 18:19:24 -0400

    Update

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,5 +1,9 @@
+import { useContext } from "react";
+import { TaskContext } from "../../contexts/TaskContext";
 import styles from "./styles.module.css";
 
 export function CounterDown() {
+  const taskContext = useContext(TaskContext);
+  console.log(taskContext);
   return <div className={styles.container}>00:00</div>;
 }
