d1f152858f95f21fb481c5a146c0200277ba36bb
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-06-16 18:12:54 -0400

    Form

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,16 +1,51 @@
-import { PlayCircleIcon } from "lucide-react";
-import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 import { useRef } from "react";
+import { TaskModel } from "../../models/TaskModel";
+import { Cycles } from "../Cycles";
+import { PlayCircleIcon } from "lucide-react";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function MainForm() {
+  const { setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-
     console.log("DEU CERTO");
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert("Digite o nome da tarefa");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: "workTime",
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, // Conferir
+        secondsRemaining, // Conferir
+        formattedSecondsRemaining: "00:00", // Conferir
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
 
   return (
@@ -20,13 +55,13 @@ export function MainForm() {
           labelText="task"
           id="meuInput"
           type="text"
-          placeholder="Digite Algo"
+          placeholder="Digite algo"
           ref={taskNameInput}
         />
       </div>
 
       <div className="formRow">
-        <p>Próximo intervalo é de 25 min</p>
+        <p>Próximo intervalo é de 25min</p>
       </div>
 
       <div className="formRow">
--- src/contexts/TaskContext/useTaskContext.ts
+++ src/contexts/TaskContext/useTaskContext.ts
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { TaskContext } from "./TaskContext";
 
-export function useTaskContent() {
+export function useTaskContext() {
   return useContext(TaskContext);
 }
