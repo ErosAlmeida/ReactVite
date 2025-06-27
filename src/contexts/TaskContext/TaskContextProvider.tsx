89a65ac7b9e26e78cec5b7cb8164bda7b0f4e9bd
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-06-27 14:06:31 -0400

    update

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useState } from "react";
 import { TaskContext } from "./TaskContext";
 import { initialTaskState } from "./initialTaskState";
 
@@ -9,32 +9,13 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
 
-  type ActionType = {
-    type: String;
-    payLoad?: number;
-  };
-
-  const [numero, dispatch] = useReducer((
-    state,action:ActionType) => {
-      console.log(state,action);
-    })
-
-    switch(action.type){
-      return state,
-    }
-    {
-      secondRemaning : 0;
-    }
-
-  //useEffect(() => {
-  // console.log(state);
-  //}, [state]);
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-     <h1>O estado é: {JSON.stringify(myState)}</h1>
-      <button onClick={() => dispatch({ type: 'INCREMENT', payload: 10 })}></button>
-      incrementar +10
+      {children}
     </TaskContext.Provider>
   );
 }
