76a8b76a0c967b769631a6e3d7ab3cbff271892a
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-08-03 11:45:28 -0400

    Count

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -14,10 +14,15 @@ export enum TaskActionTypes {
   COMPLETE_TASK = "COMPLETE_TASK",
 }
 
-export type TaskActionsWithPayload = {
-  type: TaskActionTypes.START_TASK;
-  payload: TaskModel;
-};
+export type TaskActionsWithPayload =
+  | {
+      type: TaskActionTypes.START_TASK;
+      payload: TaskModel;
+    }
+  | {
+      type: TaskActionTypes.COUNT_DOWN;
+      payload: { secondsRemaining: number };
+    };
 
 export type TaskActionsWithoutPayload =
   | {
@@ -25,6 +30,9 @@ export type TaskActionsWithoutPayload =
     }
   | {
       type: TaskActionTypes.INTERRUPT_TASK;
+    }
+  | {
+      type: TaskActionTypes.COMPLETE_TASK;
     };
 
 export type TaskActionModel =
