62f6b3d215581e68d9e58d9fbbbc1c7d6797c5c1
ErosAlmeida <erosvilela.empresa@gmail.com>
2025-09-12 14:16:55 -0300

    testes

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -8,7 +8,7 @@ export function Logo() {
     <div className={styles.logo}>
       <a className={styles.logoLink} href='#'>
         <div>teste</div>
-        <div>teste</div>
+       
         <TimerIcon />
         <span>Chronos</span>
       </a>
